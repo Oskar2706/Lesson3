75f3a3dffef8d65d319c1ebfd7aaaa8d37953069
Oskar2706 <alexvaraksa@bk.ru>
2022-11-26 17:30:51 +0300

    save part 5

---
+++ src/group2/part5.java
@@ -0,0 +1,13 @@
+package group2;
+
+public class part5 {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println(pos+"");
+        for (int i = 1; i < 10; i++) {
+            pos=pos-5;
+            System.out.println(pos+"");
+
+        }
+    }
+}
