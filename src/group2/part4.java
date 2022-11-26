f0ca5b1386da01f7245a65336bb2057b67820316
Oskar2706 <alexvaraksa@bk.ru>
2022-11-26 17:13:27 +0300

    save part 4

---
+++ src/group2/part4.java
@@ -0,0 +1,14 @@
+package group2;
+
+public class part4 {
+    public static void main(String[] args) {
+        int seven = 7;
+        while (seven <= 98) {
+
+            System.out.print (seven+" ");
+            seven+=7;
+
+        }
+
+    }
+}
