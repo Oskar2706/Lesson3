bab4bd4ee255d8059e9dd48a4801acb311adb182
Oskar2706 <alexvaraksa@bk.ru>
2022-11-26 15:47:24 +0300

    save part 2

---
+++ src/group2/part2.java
@@ -0,0 +1,10 @@
+package group2;
+
+public class part2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >0; i--) {
+            System.out.print(i + "");
+
+        }
+    }
+}
