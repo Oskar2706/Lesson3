b0ec37c50b56f861f709c3246b7b2c3d460f8704
Oskar2706 <alexvaraksa@bk.ru>
2022-11-26 17:46:54 +0300

    save part 6

---
+++ src/group2/part6.java
@@ -0,0 +1,11 @@
+package group2;
+
+public class part6 {
+    public static void main(String[] args) {
+        int n = 20 ;
+        for(int i = 10; i<=n; i++){
+            int result = i*i;
+            System.out.print(result +" ");
+    }
+}
+}
