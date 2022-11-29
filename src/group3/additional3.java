c5999a8629be6740abcfb967486eb9fe410bfd42
Oskar2706 <alexvaraksa@bk.ru>
2022-11-29 14:53:50 +0300

    add  3

---
+++ src/group3/additional3.java
@@ -0,0 +1,18 @@
+package group3;
+
+public class additional3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+
+            for (int j = 1; j < 10; j++) {
+
+                System.out.print(i * j);
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
